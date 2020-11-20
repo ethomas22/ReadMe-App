d32f307e4a1e78c5a18a2e0f6d0134604d99ef53
Emily Thomas <ethomas@quint.co.uk>
2020-11-20 00:22:24 -0800

    added questions, and completed init function

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown');
+
 // array of questions for user
-const questions = [
+const questions = [{
+      type: 'input',
+      message: 'What is your ReadMe title?',
+      name: 'title'
+},
+{
+    type: 'input',
+    message: 'What is the description of your ReadMe?',
+    name: 'description'
+},
+{
+    type: 'input',
+    message: 'What is the installation command?',
+    name: 'installation'
+},
+{
+    type: 'input',
+    message: 'Enter usage information',
+    name: 'usage'
+},
+{
+    type: 'input',
+    message: 'Enter contributing guidelines',
+    name: 'contribution'
+},
+{
+    type: 'input',
+    message: 'What is the test command?',
+    name: 'testing'
+},
+{
+    type: 'list',
+    message: 'Choose a license for your project',
+    name: 'license',
+    choices: ['MIT', 'Apache', 'GPL v3.0', 'Eclipse']
+}
 
 ];
 
@@ -9,7 +47,10 @@ function writeToFile(fileName, data) {
 
 // function to initialize program
 function init() {
-
+    inquirer
+        .prompt(questions)
+        .then(generateMarkdown)
+        .then(console.log);
 }
 
 // function call to initialize program
