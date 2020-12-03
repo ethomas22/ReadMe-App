59f3f2604ac372fe2742fb33071d5505d00b7f21
Emily Thomas <ethomas@quint.co.uk>
2020-12-02 22:12:38 -0800

    completed markdown and ran a test readme

--- README.md
+++ README.md
@@ -1,3 +1,24 @@
-# ReadMe-App
+# emily 
+  # Table of Contents
+  1. [Description](#Description)
+  1. [Installation](#installtion)
+  1. [Usage](#Usage)
+  1. [Contributing Guidelines](#Contributing-Guidelines)
+  1. [Test Command](#Test-Command)
+  1. [License](#License)
 
-## Description
+  # Description
+   i dont care
+   # Installation
+   npm i
+   # Usage
+   no
+   # Contributing Guidelines
+   josh
+   # Test Command
+   no
+   # License 
+   MIT
+   
+
+  
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,7 +1,28 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title} 
-  # ${data.description}
+  # Table of Contents
+  1. [Description](#Description)
+  1. [Installation](#installtion)
+  1. [Usage](#Usage)
+  1. [Contributing Guidelines](#Contributing-Guidelines)
+  1. [Test Command](#Test-Command)
+  1. [License](#License)
+
+  # Description
+   ${data.description}
+   # Installation
+   ${data.installation}
+   # Usage
+   ${data.usage}
+   # Contributing Guidelines
+   ${data.contribution}
+   # Test Command
+   ${data.testing}
+   # License 
+   ${data.license}
+   
+
   
 `;
 }
